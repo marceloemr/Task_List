4be094e4bc30dd6d30aeb114c203c748b2206032
Marcelo Eduardo Marques Ribas <contato.marcelomk@hotmail.com>
2024-05-24 09:43:56 -0300

    Add Task model

---
+++ src/models/Task.ts
@@ -0,0 +1,43 @@
+export default class Task {
+    #id: number
+    #description: string
+    #done: boolean
+
+    constructor(id: number, description: string, done = false) {
+        this.#id = id
+        this.#description = description
+        this.#done = done
+    }
+
+    static create_active(id: number, description: string) {
+        return new Task(id, description)
+    }
+
+    static create_done(id: number, description: string) {
+        return new Task(id, description, true)
+    }
+
+    get id() {
+        return this.#id
+    }
+
+    get description() {
+        return this.#description
+    }
+
+    get done() {
+        return this.#done
+    }
+
+    task_done() {
+        return Task.create_done(this.id, this.description)
+    }
+
+    task_active() {
+        return Task.create_active(this.id, this.description)
+    }
+
+    switch_status() {
+        return this.done ? this.task_active() : this.task_done()
+    }
+}
